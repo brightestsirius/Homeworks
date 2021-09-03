0b1730dd9f2944b2d8edf1aafe0550b55cac3774
Volkova Katerina <volkovakate87@gmail.com>
2021-09-03 14:50:07 +0300

    HW-1

---
+++ HW/HW-1/script.js
@@ -0,0 +1,15 @@
+
+let radius = 0.7;
+let square=Math.PI *radius**2;
+// console.log(square);
+let height = 0.6;
+let volume=square*height;
+// console.log(volume);
+let stars= "************";
+let strip="---------------";
+console.log(stars);
+console.log("Обьем цилиндра с площадью основы S = ",square, "радиусом R =",radius, "и высотой H =",height, "равен:");
+console.log(strip);
+console.log("V =",volume);
+console.log(strip);
+console.log("end.")
